e82f3073262d88f4f2135f184eeb38fe88fd7a7c
LEE YURI <lyr8403@naver.com>
2020-06-10 03:15:33 +0900

    upload MyActionBar

---
+++ MyActionBar/app/src/main/java/ddwucom/mobile/myactionbar/MainActivity.java
@@ -0,0 +1,21 @@
+package ddwucom.mobile.myactionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
