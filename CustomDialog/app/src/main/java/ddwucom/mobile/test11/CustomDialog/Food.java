e3a010a048e58655d29b4f30240a6183b84dbe05
LEE YURI <lyr8403@naver.com>
2020-06-09 18:26:39 +0900

    upload CustomDialog

---
+++ CustomDialog/app/src/main/java/ddwucom/mobile/test11/CustomDialog/Food.java
@@ -0,0 +1,33 @@
+package ddwucom.mobile.test11.CustomDialog;
+
+public class Food {
+    String food;
+    String nation;
+
+    public Food(String food, String nation) {
+        this.food = food;
+        this.nation = nation;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    @Override
+    public String toString() {
+        return food + "\t\t(" + nation + ")";
+    }
+
+}
